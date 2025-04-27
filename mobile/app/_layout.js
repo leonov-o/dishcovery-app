1e5e702447aca9247e1f42bd5efcdb4291e857f5
leonov-o <oleksandr.leonov.work@gmail.com>
2025-04-27 15:59:18 +0300

    Add Tanstack Query.

--- mobile/app/_layout.js
+++ mobile/app/_layout.js
@@ -3,6 +3,9 @@ import {useFonts} from 'expo-font';
 import {Stack} from "expo-router";
 
 import * as SplashScreen from "expo-splash-screen";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const Layout = () => {
     const [loaded, error] = useFonts({
@@ -27,7 +30,11 @@ const Layout = () => {
         return null;
     }
 
-    return <Stack/>
+    return (
+        <QueryClientProvider client={queryClient}>
+          <Stack />
+        </QueryClientProvider>
+      );
 };
 
 export default Layout;
